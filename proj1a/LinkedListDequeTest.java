4d55e0a2f4ef927a82cd4c9b3ba9f102db2859e0
twangcode <twangcode@gmail.com>
2020-09-08 05:53:53 -0500

    Updated Proj 1a: fixed style issues

--- proj1a/LinkedListDequeTest.java
+++ proj1a/LinkedListDequeTest.java
@@ -34,7 +34,7 @@ public class LinkedListDequeTest {
 	  * && is the "and" operation. */
 	public static void addIsEmptySizeTest() {
 		System.out.println("Running add/isEmpty/Size test.");
-//		System.out.println("Make sure to uncomment the lines below (and delete this print statement).");
+
 //
 //		LinkedListDeque<String> lld1 = new LinkedListDeque<String>();
 //
@@ -65,7 +65,7 @@ public class LinkedListDequeTest {
 
 		System.out.println("Running add/remove test.");
 
-//		System.out.println("Make sure to uncomment the lines below (and delete this print statement).");
+
 
 //		LinkedListDeque<Integer> lld1 = new LinkedListDeque<Integer>();
 //		// should be empty
@@ -88,4 +88,4 @@ public class LinkedListDequeTest {
 		addIsEmptySizeTest();
 		addRemoveTest();
 	}
-} 
+}
