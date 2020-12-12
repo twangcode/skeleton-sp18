d4ee41a5db08dd403cc0c64b2eb1a462b242d218
Tuo <twang.code@gmail.com>
2020-12-12 14:10:00 -0600

    lab10 fixed bug

--- lab10/ArrayHeap.java
+++ lab10/ArrayHeap.java
@@ -171,29 +171,19 @@ public class ArrayHeap<T> implements ExtrinsicPQ<T> {
      */
     @Override
     public T removeMin() {
-//        /* TODO: Your code here! */
-//        if (size() < 1) {
-//            return null;
-//        }
-//        T value = contents[1].item();
-//        swap(1, size);
-//        contents[size] = null;
-//        if (size > 0) {
-//        	sink(1);
-//        }
-//        size -= 1;
-//        return value;
-        int last = size();
-        T removed = contents[1].item();
-
-        swap(1, last);
-        contents[last] = null;  // Nulling out the removed node
+        /* TODO: Your code here! */
+        if (size() < 1) {
+            return null;
+        }
+        T value = contents[1].item();
+        swap(1, size);
+        contents[size] = null;
         size -= 1;
         if (size > 0) {
-            sink(1);            // Sink the promoted node
+        	sink(1);
         }
 
-        return removed;
+        return value;
     }
 
     /**
